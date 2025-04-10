81cd13bf5e3906705126fc625c83529db5f53b18
BinaryWiz4rd <278437@student.pwr.edu.pl>
2025-04-10 11:41:15 +0200

    extended admin & fixed binding

--- app/src/main/java/com/example/project/Doctor/UsersAdapter.kt
+++ app/src/main/java/com/example/project/Doctor/UsersAdapter.kt
@@ -1,4 +1,4 @@
-package com.example.project.Doctor
+/*package com.example.project.Doctor
 
 import android.view.LayoutInflater
 import android.view.View
@@ -28,4 +28,4 @@ class UsersAdapter(private val users: List<User>) : RecyclerView.Adapter<UsersAd
     }
 
     override fun getItemCount(): Int = users.size
-}
+}*/
