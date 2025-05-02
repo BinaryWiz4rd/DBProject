fc7fb775a802573d1eb468473fa8c4f912e09f73
BinaryWiz4rd <278437@student.pwr.edu.pl>
2025-05-02 18:48:43 +0200

    added home functionality, class patient handled

---
+++ app/src/main/java/com/example/project/Doctor/DoctorHomeFragment.kt
@@ -0,0 +1,18 @@
+package com.example.project.Doctor
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.project.R
+
+class DoctorHomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_doctor_home, container, false)
+    }
+}
